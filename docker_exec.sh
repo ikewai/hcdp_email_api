950ad28c6b66db185983979d60bd445eea4f3c85
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-06-15 17:30:05 -1000

    user

--- docker_exec.sh
+++ docker_exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t hcdp_email_api .
-docker run -d -p 443:443 -v /mnt/netapp/ikewai/annotated/Rainfall:/data hcdp_email_api
+docker run -u ikewai -d -p 443:443 -v /mnt/netapp/ikewai/annotated/Rainfall:/data hcdp_email_api
