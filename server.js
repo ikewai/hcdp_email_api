ac1835a33a3724acf6f90f5ee25bba2debe6eca5
princessbobsanta <princessbobsantaclaus@gmail.com>
2021-06-17 12:29:53 -1000

    cors header access

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Range");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Range, Content-Range");
   //pass to next layer
   next();
 });
