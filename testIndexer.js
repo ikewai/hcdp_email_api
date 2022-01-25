a137e14fb8121badf04b58e15f7514bc8ba433eb
jardmclan <mcleanj@hawaii.edu>
2022-01-25 09:29:32 -1000

    test indexer

--- testIndexer.js
+++ testIndexer.js
@@ -1,8 +1,24 @@
 
+const https = require("https");
+
 let types = [{
     datatype: "rainfall",
     properties: {
         
+    },
+    files: [],
+    range: {
+        start: "2010-12-01",
+        end: "2010-12-01",
+        period: null
     }
-    files: []
-}]
+}];
+
+
+let periods = ["day", "month"];
+let datatypes = ["rainfall", "temperature"];
+let fills = ["filled", "partial", "unfilled"];
+let aggregations = ["min", "max", "avg"];
+let extents = ["statewide", "bi", "ka", "mn", "oa"];
+let tiers = ["archival"];
+let productions = ["new", "legacy"];
