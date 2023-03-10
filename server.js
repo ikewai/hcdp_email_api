02617e1de858d3759a6c8f4648e47edde45de568
princessbobsanta <princessbobsantaclaus@gmail.com>
2023-03-10 12:42:41 -1000

    test

--- server.js
+++ server.js
@@ -961,7 +961,7 @@ function signBlob(key, blob) {
 }
 
 //add github middleware with secret, doesn't use any user input but don't necessarily want this running arbitrarily and shouldn't need to
-app.post("/addmetadata", githubMiddleware, async (req, res, buffer) => {
+app.post("/addmetadata", async (req, res, buffer) => {
   try {
     const receivedSig = req.headers['x-hub-signature'];
     const computedSig = signBlob(githubWebhookSecret, buffer);
