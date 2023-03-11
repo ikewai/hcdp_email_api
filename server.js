b1048b901b3b6178db74678bd10cd613b8d58db3
princessbobsanta <princessbobsantaclaus@gmail.com>
2023-03-10 16:19:21 -1000

    test

--- server.js
+++ server.js
@@ -981,7 +981,7 @@ function signBlob(key, blob) {
 }
 
 //add github middleware with secret, doesn't use any user input but don't necessarily want this running arbitrarily and shouldn't need to
-app.post("/addmetadata", bodyParser.json({
+app.post("/addmetadata", bodyParser.raw({
   limit: "10mb",
   verify: (req, res, buf) => {
     req.rawBody = buf;
