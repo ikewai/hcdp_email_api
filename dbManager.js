29ebbcfe5967b1b2c797fdc8df4e3cbfe557a008
princessbobsanta <princessbobsantaclaus@gmail.com>
2023-03-10 19:15:01 -1000

    remove debugging metadata working

--- dbManager.js
+++ dbManager.js
@@ -266,16 +266,13 @@ class TapisManager {
                 }
                 //if they are not identical replace doc with the new one (otherwise do nothing)
                 if(!identical) {        
-                    console.log("replace");
                     await this.dbManager.replaceRecord(existingDoc.uuid, doc.value);
                 }
             }
             else {     
-                console.log("create"); 
                 await this.create(doc);
             }
         }
-        console.log("done!!");
     }
 }
 
