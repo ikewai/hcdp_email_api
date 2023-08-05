b33fc6564562fbd474bc5e5d856119f483203e71
princessbobsanta <princessbobsantaclaus@gmail.com>
2023-08-04 14:43:09 -1000

    debug

--- fileIndexer.js
+++ fileIndexer.js
@@ -54,6 +54,7 @@ async function getPaths(root, data, collapse) {
                     let fdirType = path.join(fdir, ftype);
                     let start = new moment(range.start);
                     let end = new moment(range.end);
+                    console.log(start, end);
                     let pathData = await getPathsBetweenDates(fdirType, start, end, collapse);
                     totalFiles += pathData.numFiles;
                     paths = paths.concat(pathData.paths);
