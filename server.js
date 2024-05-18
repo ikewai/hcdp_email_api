a958003e648f6eb0cb73e0961e2869c4a9487e16
jardmclan <mcleanj@hawaii.edu>
2024-05-17 17:16:50 -1000

    fix zip name ref

--- server.js
+++ server.js
@@ -1475,7 +1475,7 @@ async function createMesonetPackage(stationIDs, combine, ftype, csvMode, options
     fpath = files[0];
   }
   else {
-    let zipProc = child_process.spawn("sh", ["./zipgen.sh", downloadRoot, productionRoot, zipName, files]);
+    let zipProc = child_process.spawn("sh", ["./zipgen.sh", downloadRoot, productionRoot, "data.zip", files]);
 
     //write stdout (should be file name) to output accumulator
     let code = await handleSubprocess(zipProc, (data) => {
@@ -1544,7 +1544,6 @@ app.get("/mesonet/createPackage/link", async (req, res) => {
     catch(e) {
       return processTapisError(res, reqData, e);
     }
-    
 
     reqData.code = 200;
     res.status(200)
